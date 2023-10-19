c809ecae3d1844f1b6f41451acb4a7fb3b29115e
knight <143659423+RegularKnight@users.noreply.github.com>
2023-10-19 11:56:17 +0300

    Add files via upload

--- necklace.py
+++ necklace.py
@@ -1,10 +1,11 @@
 # -*- coding:utf -8 -*-
 #!/usr/bin/python3
 import random
-print("> Necklace V1 <")
+print("> Necklace V2 <")
 s = '123412343718372831234===---___QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm==-#@ajdkajfie'
 o = 1
-l = 110
+l = input('Password length?'+ "\n")
+l = int(l)
 for n in range(o):
     p =''
     for i in range(l):
